2bf4d4e03d98a9055fea50be053b5aadfbf1b69c
athomasprojects <amanda.thomas14@gmail.com>
2025-10-20 11:05:25 -0400

    add modtap lgui to left B key, cleanup formatting

--- keyboards/monsgeek/m6/keymaps/amanda/keymap.c
+++ keyboards/monsgeek/m6/keymaps/amanda/keymap.c
@@ -86,10 +86,10 @@ enum __layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT( /* Base */
-        KC_GRV,        KC_1,             KC_2,               KC_3,               KC_4,               KC_5, KC_6, KC_7,               KC_8,    KC_9,  KC_0,    KC_MINS,               KC_EQL,  KC_BSPC,                KC_DEL,
-        LT(_POG,       KC_TAB),          KC_Q,               KC_W,               KC_E,               KC_R, KC_T, KC_Y,               KC_U,    KC_I,  KC_O,    KC_P,                  KC_LBRC, KC_RBRC,       KC_BSLS, KC_PGUP,
-        KC_ESC,        MT(MOD_CA, KC_A), KC_S,               KC_D,               MT(MOD_LALT, KC_F), KC_G, KC_H, MT(MOD_LALT, KC_J), LT(_POG, KC_K), KC_L,    MT(MOD_LGUI, KC_SCLN), KC_QUOT, KC_NUHS,       KC_ENT,  KC_PGDN,
-        OSM(MOD_LSFT), KC_NUBS,          MT(MOD_LCTL, KC_Z), MT(MOD_CSHF, KC_X), KC_C,               KC_V, KC_B, KC_B,               KC_N,    KC_M,  KC_COMM, KC_DOT,                KC_SLSH, OSM(MOD_RSFT), KC_UP,
+        KC_GRV,        KC_1,             KC_2,               KC_3,               KC_4,               KC_5, KC_6,               KC_7,               KC_8,    KC_9,  KC_0,    KC_MINS,               KC_EQL,  KC_BSPC,                KC_DEL,
+        LT(_POG,       KC_TAB),          KC_Q,               KC_W,               KC_E,               KC_R, KC_T,               KC_Y,               KC_U,    KC_I,  KC_O,    KC_P,                  KC_LBRC, KC_RBRC,       KC_BSLS, KC_PGUP,
+        KC_ESC,        MT(MOD_CA, KC_A), KC_S,               KC_D,               MT(MOD_LALT, KC_F), KC_G, KC_H,               MT(MOD_LALT, KC_J), LT(_POG, KC_K), KC_L,    MT(MOD_LGUI, KC_SCLN), KC_QUOT, KC_NUHS,       KC_ENT,  KC_PGDN,
+        OSM(MOD_LSFT), KC_NUBS,          MT(MOD_LCTL, KC_Z), MT(MOD_CSHF, KC_X), KC_C,               KC_V, MT(MOD_LGUI, KC_B), KC_B,               KC_N,    KC_M,  KC_COMM, KC_DOT,                KC_SLSH, OSM(MOD_RSFT), KC_UP,
         MT(MOD_SUPCSH, KC_CAPS), KC_LALT, OSM(MOD_LSFT), KC_LCTL,                                                KC_SPC, KC_RWIN, TT(_FN), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_POG] = LAYOUT( /* Base */
